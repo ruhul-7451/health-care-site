b102dca280fcea7bcd70f2ac0fcd4ae3323b0865
ruhul-7451 <csehastakenmylife@gmail.com>
2021-10-18 16:15:57 +0600

    project started

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7WfPkPuNTawuVUO3jofPKHqtDfyfsKZY",
+    authDomain: "e-sheba-5c4f5.firebaseapp.com",
+    projectId: "e-sheba-5c4f5",
+    storageBucket: "e-sheba-5c4f5.appspot.com",
+    messagingSenderId: "525656644968",
+    appId: "1:525656644968:web:9974234815020b598a8add"
+};
+
+export default firebaseConfig;
